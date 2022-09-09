3e7292c6de1ab30add2db8256b7aa534aa94c268
eddieton <eddieton@gmail.com>
2022-09-08 19:05:27 -0700

    hello put task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main- Serves as the entry point for this program
+ *
+ * RETURN: By default returns zero if no erros otherwise returns non-zero value.*/
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle"):
+		return(0);
+}
